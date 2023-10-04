505f676c52d4d803434c084b08711bf09a96ac82
Rashaad <rashaadzma@gmail.com>
2023-10-04 21:51:10 +0530

    Initial commit

---
+++ frontend/src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'DSMUN Official Website';
+export const SITE_DESCRIPTION = `Welcome to the official website of the
+                                 D. S. Senanayake College Model United Nations (MUN) club`;
