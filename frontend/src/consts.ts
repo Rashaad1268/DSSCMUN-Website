74256b712ccb3e9a9b00a6ef174d0e5a9318a894
Rashaad <rashaadzma@gmail.com>
2023-10-06 22:07:20 +0530

    Setup everything

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -1,6 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'DSMUN Official Website';
-export const SITE_DESCRIPTION = `Welcome to the official website of the
-                                 D. S. Senanayake College Model United Nations (MUN) club`;
+export const SITE_TITLE = 'Official Website Of DSMUN';
+export const SITE_DESCRIPTION = `The Model United Nations Club of D. S. Senanayake College
+is a student-led organization that provides students with the opportunity to learn about
+global affairs and diplomacy through simulated United Nations committee meetings.`;
+
+export const twitter_link = '';
+export const facebook_link = '';
+export const contact_email = '';
