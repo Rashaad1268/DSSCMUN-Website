f07c61ffc8b17c665411db39aaf5589ed80852e4
Rashaad <rashaadzma@gmail.com>
2024-05-12 00:55:51 +0530

    major changes

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
+import { astroImageTools } from "astro-imagetools";
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), prefetch({
-    intentSelector: 'a[href]',
-  })]
+  integrations: [mdx(), sitemap(), tailwind(), prefetch({intentSelector: 'a[href]'}), astroImageTools]
 });
--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -1,11 +1,9 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'Official Website Of DSMUN';
 export const SITE_DESCRIPTION = `The Model United Nations Club of D. S. Senanayake College
 is a student-led organization that provides students with the opportunity to learn about
 global affairs and diplomacy through simulated United Nations committee meetings.`;
 
-export const twitter_link = '';
-export const facebook_link = '';
+export const youtube_link = 'https://www.youtube.com/@dssc_model.united.nations';
+export const twitter_link = 'https://twitter.com/dsscmun20230620';
+export const instagram_link = 'https://www.instagram.com/dsscmun/';
 export const contact_email = '';
